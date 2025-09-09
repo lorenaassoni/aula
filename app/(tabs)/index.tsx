ff413803b01050a19115e1cbbb7cda7bbd968cc6
unknown <lorena.assoni@preve.g12.br>
2025-09-09 16:41:00 -0300

    atualizacao dos gestos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('@/assets/images/imagemsite.png');
 
@@ -50,7 +51,7 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text style={styles.text}>Bem-Vindo ao Nosso Site!</Text>
       <Text style={styles.text}>Seja membro!</Text>
 
@@ -76,7 +77,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
