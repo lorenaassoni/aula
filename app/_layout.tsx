9d84fb96a91a5dfcb32775cd6550fab6916a6baf
unknown <lorena.assoni@preve.g12.br>
2025-09-23 16:46:08 -0300

    atualizando

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,12 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { LogBox } from "react-native";
 
+
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
     <>
-    <StatusBar style="light"/>
       <Stack
       screenOptions={{
         headerStyle: { 
@@ -20,6 +20,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
         <Stack.Screen name="+not-fount" options={{}} />
        </Stack>
+       <StatusBar style="light"/>
      </>
   );
 }
